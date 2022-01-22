2ef08c17383f25ded83c4d9f6bfa3014b62e5f04
suman <sumall@utu.fi>
2022-01-23 00:14:26 +0200

    working with api

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./NewExpense/NewExpense";
 
-let expensesDummy = [
-  //This is used to initialize only
-  {
-    id: "e1",
-    title: "Transportation",
-    amount: "100",
-    date: new Date(2022, 1, 20),
-  },
-  {
-    id: "e2",
-    title: "Internet",
-    amount: "50",
-    date: new Date(2022, 1, 1),
-  },
-  {
-    id: "e3",
-    title: "Food and Groceries",
-    amount: "250",
-    date: new Date(2022, 1, 8),
-  },
-  {
-    id: "e4",
-    title: "Drinks",
-    amount: "150",
-    date: new Date(2022, 1, 25),
-  },
-];
+let expensesDummy = [];
 
 function App() {
   const [expenses, setExpenses] = useState(expensesDummy);
+
+  // useEffect doesn't allow to run on a loop. Blank dependency will allow to run once only.
+  //Only run if there's any change in dependency array
+   function fetchData(){
+      fetch("https://localhost/sample-api/api/")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setExpenses(data);
+        });
+  useEffect(() => {
+   fetchData();
+    
+  }, []);
+  // Basic Fetch Request
+  // fetch('http://example.com/movies.json). This can be any URL
+  // .then(response => response.json). All the responses in JSON
+  //.then(data=> console.log(data));   Actual data will be retrieved
 
   //Similarly, App is parent for NewExpense so creating a function so that it can be passed
   //creating a vraiable in the parameter, this paramter recieves the data
   //Expense is the new object and expenses is the old object, copied with spread operator
   const addExpenseHandler = (expense) => {
-    const updatedExpense = [expense, ...expenses];
-    setExpenses(updatedExpense); //This way value can be updated
+    {
+      /* const updatedExpense = [expense, ...expenses];
+    setExpenses(updatedExpense); //This way value can be updated*/
+    }
+    fetch("https://localhost/sample-api/api/", {
+      method: "POST",
+      body: JSON.stringify(expense),
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then( response => {
+      fetchData();
+    }
+    );
   };
   return (
     <div>
